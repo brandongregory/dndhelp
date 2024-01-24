0871e94478bb3ffdb6b6b2181f459590953dd823
Brandon Gregory <brandon.gregory.1981@gmail.com>
2024-01-24 13:12:18 -0600

    Adding monk calculator

---
+++ src/5e-tools/index.md
@@ -0,0 +1,11 @@
+---
+layout: table-of-contents.njk
+meta_title: "DnD 5e Tools"
+meta_description: "Calculators and other tools for DnD 5e"
+author: "Brandon Gregory"
+section_name: tools
+tags: ['main-page']
+date: Last Modified
+---
+
+{{ meta_description }}
--- src/js/custom.js
+++ src/js/custom.js
@@ -37,3 +37,51 @@ function closeAllNavItems() {
         subnavParent.classList.remove("open")
     });
 }
+
+function initDeflectMissiles() {
+    const monkLevelField = document.querySelector("#deflect-missiles--monk-level");
+    const dexterityModifierField = document.querySelector("#deflect-missiles--dexterity-modifier");
+
+    monkLevelField.addEventListener("input", function() {
+        calculateDeflectMissiles();
+    });
+    dexterityModifierField.addEventListener("input", function() {
+        calculateDeflectMissiles();
+    });
+}
+
+function calculateDeflectMissiles() {
+    let monkLevel = document.querySelector("#deflect-missiles--monk-level").value;
+    let dexterityModifier = document.querySelector("#deflect-missiles--dexterity-modifier").value;
+    const averageDamageField = document.querySelector("#deflect-missiles--average-damage");
+    const minimumDamageField = document.querySelector("#deflect-missiles--average-damage");
+    const averageRoll = 5.5;
+    const minimumRoll = 1;
+
+    if (isNaN(monkLevel)) {
+        monkLevel = 0;
+    } else {
+        monkLevel = Number(monkLevel);
+    }
+
+    if (isNaN(dexterityModifier)) {
+        dexterityModifier = 0;
+    } else {
+        dexterityModifier = Number(dexterityModifier);
+    }
+
+    const averageDamage = averageRoll + monkLevel + dexterityModifier;
+    const minimumDamage = minimumRoll + monkLevel + dexterityModifier;
+
+    averageDamageField.innerHTML = averageDamage;
+    minimumDamageField.innerHTML = minimumDamage;
+}
+
+function initPageScripts() {
+    const body = document.querySelector("body");
+    if (body.classList.contains("monk-deflect-missiles")) {
+        initDeflectMissiles();
+    }
+}
+
+initPageScripts();
